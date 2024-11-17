6adb0e237997393b2fbe734c483e4592f6f46e64
chs98412 <chs98412@gmail.com>
2024-11-17 23:46:59 +0900

    token

--- src/main/kotlin/com/yedongsoon/accounts/OAuth2AuthenticationSuccessHandler.kt
+++ src/main/kotlin/com/yedongsoon/accounts/OAuth2AuthenticationSuccessHandler.kt
@@ -35,7 +35,7 @@ class OAuth2AuthenticationSuccessHandler(
                     profilePhoto=profileImage,
             )).no
         }
-        val token = jwtTokenUtil.generateToken("kakao", email, nickname, profileImage)
+        val token = jwtTokenUtil.generateToken("kakao", email, nickname, profileImage,memberNo)
 
         println("request.remoteAddr = ${request.remoteAddr}")
         //val url = if(request.remoteAddr.contains("*158*")) makeRedirectUrl(token) else makeLocalRedirectUrl(token)
